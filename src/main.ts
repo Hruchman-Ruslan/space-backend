d2c28c8cd31ac1f653b3646549b754bd9be15442
Ruslan <Giovingo92@gmail.com>
2024-03-27 19:57:07 +0200

    feat: auth

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 const { PORT } = process.env;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT, () => {
     console.log(`Server running. Use our API on port ${PORT}`);
